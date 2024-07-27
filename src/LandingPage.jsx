6713a071ca5566753b31d5922ed4bc9e94d81282
Sritaj Patel <sritajp@gmail.com>
2024-07-28 01:08:37 +0530

    Added onClick event to CardButton component to redirect to page specified

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CardButton from "./components/CardButton";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const handleCardClick = (page) => {
+    navigate("/page1");
+  };
+  const handleClick = () => {
+    navigate("/about");
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-gray-100 flex items-center justify-center">
       <div className="max-w-screen-xl m-4 sm:m-10 bg-white shadow-lg rounded-lg flex flex-col items-center p-8">
@@ -9,12 +19,12 @@ const LandingPage = () => {
           Authentication Supremacy
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4 justify-center">
-          <CardButton text="Page 1" />
-          <CardButton text="Page 2" />
-          <CardButton text="Page 3" />
-          <CardButton text="Page 4" />
-          <CardButton text="Page 5" />
-          <CardButton text="Page 6" />
+          <CardButton text="Page 1" onClick={() => handleCardClick("/page1")} />
+          {/* <CardButton text="Page 2" onClick={() => handleCardClick("/page2")} />
+          <CardButton text="Page 3" onClick={() => handleCardClick("/page3")} />
+          <CardButton text="Page 4" onClick={() => handleCardClick("/page4")} />
+          <CardButton text="Page 5" onClick={() => handleCardClick("/page5")} />
+          <CardButton text="Page 6" onClick={() => handleCardClick("/page6")} /> */}
         </div>
       </div>
     </div>
