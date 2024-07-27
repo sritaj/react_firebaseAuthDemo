8b7ddf6b17b5039f9a95a75d223c1ca65ca07152
Sritaj Patel <sritajp@gmail.com>
2024-07-28 02:58:16 +0530

    Added toast Container to enable showing Toast messages and limited to display 3 at a time

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import LandingPage from "./LandingPage";
 import LoginWithUsernameAndPassword from "./pages/LoginWithUsernameAndPassword";
 import ErrorPage from "./pages/ErrorPage"; // Import the ErrorPage component
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
         <Route path="*" element={<ErrorPage />} />{" "}
         {/* Add a wildcard route for handling errors */}
       </Routes>
+      <ToastContainer limit={3} />
     </Router>
   );
 };
