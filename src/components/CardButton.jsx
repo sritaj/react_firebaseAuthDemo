8b10762d0b76e74e0f31327c2e9f9d64d1882099
Sritaj Patel <sritajp@gmail.com>
2024-07-28 00:38:57 +0530

    Made CardButton as separate component

---
+++ src/components/CardButton.jsx
@@ -0,0 +1,9 @@
+const CardButton = ({ text }) => {
+  return (
+    <div className="m-4 p-6 bg-gradient-to-r from-green-400 to-blue-500 text-white rounded-lg shadow-lg hover:from-green-500 hover:to-blue-600 transform hover:scale-105 transition-transform duration-300 cursor-pointer break-words">
+      {text}
+    </div>
+  );
+};
+
+export default CardButton;
