d24087e7fafc2038dfb7ac0aa6ed93186f99d321
Sritaj Patel <sritajp@gmail.com>
2024-07-28 00:39:24 +0530

    Added router in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./LandingPage";
+import Page1 from "./pages/Page1";
 
 const App = () => {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/page1" element={<Page1 />} />
+      </Routes>
+    </Router>
+  );
 };
+
 export default App;
