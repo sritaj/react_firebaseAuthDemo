5aee9a04719ddedb657946b27d965fbd19d09062
Sritaj Patel <sritajp@gmail.com>
2024-08-01 17:38:42 +0530

    Made the structure similar to existing page

--- src/pages/CreateSignInAccount.jsx
+++ src/pages/CreateSignInAccount.jsx
@@ -30,24 +30,34 @@ const CreateSignInAccount = () => {
         <div className="p-6  bg-white rounded-lg shadow-lg">
           <h1 className="text-2xl font-bold mb-4">Create a new account</h1>
           <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border border-gray-300 rounded-md px-4 py-2"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="border border-gray-300 rounded-md px-4 py-2"
-            />
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-2">
+                Email:
+              </label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block mb-2">
+                Password:
+              </label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="border border-gray-300 rounded-md px-4 py-2"
+              />
+            </div>
             <button
               disabled={loading}
               type="submit"
-              className="bg-blue-500 text-white rounded-md px-4 py-2"
+              className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
             >
               Create account
             </button>
